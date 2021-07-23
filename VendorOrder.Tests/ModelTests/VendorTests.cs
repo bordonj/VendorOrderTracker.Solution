15a5120d948844d6888bb866f0f697b2f6a8a697
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 10:20:06 -0700

    add test for vendor constructor instance

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test name", "test description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    
+  }
+}
