a694fcb46dc8fe4ea6f5827d74bd30ca78f4ce85
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 12:44:49 -0700

    add logic to pass constructor/properties tests for order class

--- VendorOrder/Models/Order.cs
+++ VendorOrder/Models/Order.cs
@@ -5,28 +5,26 @@ namespace VendorOrder.Models
   public class Order
   {
     public string Description { get; set; }
+    public string Title { get; set; }
+    public string Date { get; set; }
+    public int Price { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
-    public Order(string description)
+    public Order(string title, string description, string date, int price)
     {
+      Title = title;
       Description = description;
+      Date = date;
+      Price = price;
       _instances.Add(this);
       Id = _instances.Count;
     }
-
-    public static List<Order> GetAll()
-    {
-      return _instances;
-    }
-
     public static void ClearAll()
     {
       _instances.Clear();
     }
-    public static Order Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+
+
   }
 }
