2d29755d01e3e7f23211e8f0d71226b8ecf01e24
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 15:08:09 -0700

    add OrderController.cs

---
+++ VendorOrder/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using VendorOrder.Models;
+using System.Collections.Generic;
+
+namespace VendorOrder.Controllers
+{
+  public class OrderController : Controller
+  {
+
+    [HttpGet("/vendor/{vendorId}/order/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    // [HttpPost("/items/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Item.ClearAll();
+    //   return View();
+    // }
+
+    [HttpGet("/vendor/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+  }
+}
