1841c771e759dd8bf86fcb74cce314e8f8ce2128
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 10:45:37 -0700

    add tests for name, description, and id in vendor class

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -20,6 +20,40 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("test name", "test description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetNameAndDescription_ReturnsNameAndDescription_String()
+    {
+      //Arrange
+      string name = "Test Name";
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
+
+      //Act
+      string resultName = newVendor.Name;
+      string resultDescription = newVendor.Description;
+
+      //Assert
+      Assert.AreEqual(name, resultName);
+      Assert.AreEqual(description, resultDescription);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      //Arrange
+      string name = "Test Name";
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
+
+      //Act
+      int result = newVendor.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+
     
   }
 }
