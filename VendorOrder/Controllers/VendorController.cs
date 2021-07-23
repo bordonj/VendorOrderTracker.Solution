732a19b4fd8ab809c74a3c37937bc6acdaa5a860
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 15:05:40 -0700

    update VendorController for paths on ui

--- VendorOrder/Controllers/VendorController.cs
+++ VendorOrder/Controllers/VendorController.cs
@@ -13,6 +13,43 @@ namespace VendorOrder.Controllers
     List<Vendor> allCategories = Vendor.GetAll();
       return View(allCategories);
     }
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendor")]
+    public ActionResult Create(string vendorName, string description)
+    {
+      Vendor newVendor = new Vendor(vendorName, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendor/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    // This one creates new Items within a given Category, not new Categories:
+
+    [HttpPost("/vendor/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string title, string description, string date, string price)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(title, description, date, price);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
 
   }
 }
