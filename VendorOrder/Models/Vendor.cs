98fce1aeff9166359ccba39cda9993b28d71f7c5
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-23 09:42:58 -0700

    add VendorController.cs and update Vendor.cs

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -1,40 +1,42 @@
 using System.Collections.Generic;
-using System;
-using Microsoft.AspNetCore.Mvc;
-using VendorOrder.Models;
 
-namespace VendorOrder.Controllers
+namespace ToDoList.Models
 {
-  public class VendorController : Controller
+  public class Category
   {
-    [HttpGet("/vendor")]
-    public ActionResult Index()
-    {
-    // List<Vendor> allCategories = Category.GetAll();
-    //   return View(allCategories);
-    }
-    // [HttpGet("/vendor/new")]
-    // public ActionResult New()
-    // {
-    //   return View();
-    // }
-    // [HttpPost("/categories")]
-    // public ActionResult Create(string categoryName)
-    // {
-    //   Category newCategory = new Category(categoryName);
-    //   return RedirectToAction("Index");
-    // }
+    private static List<Category> _instances = new List<Category> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Item> Items { get; set; }
 
-    // [HttpGet("/categories/{id}")]
-    // public ActionResult Show(int id)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Category selectedCategory = Category.Find(id);
-    //   List<Item> categoryItems = selectedCategory.Items;
-    //   model.Add("category", selectedCategory);
-    //   model.Add("items", categoryItems);
-    //   return View(model);
-    // }
+    public Category(string categoryName)
+    {
+      Name = categoryName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Items = new List<Item>{};
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Category> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddItem(Item item)
+    {
+      Items.Add(item);
+    }
 
   }
 }
+
